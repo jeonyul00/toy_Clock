bbf85f68e40660962f4d161987eee685bf65857b
jeonyul <jeonyul00@gmail.com>
2024-10-10 13:23:00 +0900

    first commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by 전율 on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
