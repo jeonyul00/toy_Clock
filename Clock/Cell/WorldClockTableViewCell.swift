2d8fd28ce00bcfb6e4684be8b1ae09862744aec9
jeonyul <jeonyul00@gmail.com>
2024-10-14 15:38:01 +0900

    feat: delete

--- Clock/Cell/WorldClockTableViewCell.swift
+++ Clock/Cell/WorldClockTableViewCell.swift
@@ -13,11 +13,14 @@ class WorldClockTableViewCell: UITableViewCell {
     @IBOutlet weak var timeZoneLabel: UILabel!
     @IBOutlet weak var timePeriodLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet var spacingContaint: NSLayoutConstraint!
+    @IBOutlet weak var timeLabelTrailingConstaint: NSLayoutConstraint!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+     
     }
+        
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -25,4 +28,31 @@ class WorldClockTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    // tableView에서 편집 상태가 바뀔 때 마다 호출
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+                
+        if (self.superview as? UITableView) == nil { return }
+        
+        UIView.animate(withDuration: 0.4, animations: {
+            if self.timeLabel.isHidden && self.timePeriodLabel.isHidden  {
+                self.timeLabel.isHidden = editing
+                self.timePeriodLabel.isHidden = editing
+            }
+            self.spacingContaint.isActive = !editing
+            self.timeLabelTrailingConstaint.constant = editing ? -40 : 0
+
+            // 먼저 opacity 애니메이션을 적용
+            self.timeLabel.layer.opacity = editing ? 0 : 1
+            self.timePeriodLabel.layer.opacity = editing ? 0 : 1
+            self.layoutIfNeeded()
+        }, completion: { isCompletion in
+            // 애니메이션이 끝난 뒤에 isHidden 적용
+            if isCompletion {
+                self.timeLabel.isHidden = editing
+                self.timePeriodLabel.isHidden = editing
+            }
+        })
+    }
+    
 }
--- Clock/WorldClockViewController.swift
+++ Clock/WorldClockViewController.swift
@@ -6,11 +6,9 @@
 //
 
 import UIKit
-
+// MARK: - property
 class WorldClockViewController: UIViewController {
-    
     @IBOutlet weak var tableView: UITableView!
-    
     var list = [
         TimeZone(identifier: "Asia/Seoul")!,
         TimeZone(identifier: "Europe/Paris")!,
@@ -19,6 +17,7 @@ class WorldClockViewController: UIViewController {
         TimeZone(identifier: "Asia/Vladivostok")!,
     ]
     
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
@@ -31,9 +30,18 @@ class WorldClockViewController: UIViewController {
                 self.tableView.reloadData()
             }
         }
-    }    
+        self.navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.setEditing(editing, animated: animated)
+        
+    }
 }
 
+// MARK: - tableView
 extension WorldClockViewController:UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
@@ -41,12 +49,19 @@ extension WorldClockViewController:UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "WorldClockTableViewCell", for: indexPath) as! WorldClockTableViewCell
-        
         let target = list[indexPath.row]
         cell.timeLabel.text = target.currentTime
         cell.timePeriodLabel.text = target.timePeriod
         cell.timeZoneLabel.text = target.city
         cell.timeOffCellLabel.text = target.timeOffset
         return cell
+    }
+    
+    // 편집 기능: 셀을 왼쪽으로 스와이프하면 삭제 버튼 표시 && 탭하면 호출
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            list.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic) // 삭제 애니메이션 추가
+        }
     }
 }
