ba685b05ea9f971782213cbc54677fba14e9be37
jeonyul <jeonyul00@gmail.com>
2024-10-14 11:13:46 +0900

    update

--- Clock/CitySelectionViewController.swift
+++ Clock/CitySelectionViewController.swift
@@ -17,12 +17,11 @@ struct Section {
     let items: [Item]
 }
 
-
+// MARK: - property
 class CitySelectionViewController: UIViewController {
-    
     @IBOutlet weak var tableView: UITableView!
     var list = [Section]()
-    
+    var filteredList = [Section]()
     func setupList() {
         var dict = [String: [TimeZone]]()
         
@@ -47,34 +46,58 @@ class CitySelectionViewController: UIViewController {
             list.append(section)
         }
         list.sort { $0.title < $1.title }
+        filteredList = list
     }
-    
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "검색"
+        searchBar.delegate = self
+        
+        let btn = UIButton(type: .custom)
+        btn.setTitle("cancle", for: .normal)
+        btn.setTitleColor(.systemOrange, for: .normal)
+        btn.addTarget(self, action: #selector(closeVC), for: .touchUpInside)
+        btn.widthAnchor.constraint(greaterThanOrEqualToConstant: 40).isActive = true
+        
+        let stack = UIStackView(arrangedSubviews: [searchBar, btn])
+        stack.axis = .horizontal
+        stack.spacing = 8
+        stack.widthAnchor.constraint(greaterThanOrEqualToConstant: view.bounds.width - 40).isActive = true
+        navigationItem.titleView = stack
+        
         setupList()
         tableView.reloadData()
     }
     
+    @objc func closeVC() {
+        self.dismiss(animated: true)
+        
+    }
+    
 }
 
+// MARK: - tableView
 extension CitySelectionViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return list.count
+        return filteredList.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list[section].items.count
+        return filteredList[section].items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell", for: indexPath)
-        let target = list[indexPath.section].items[indexPath.row]
+        let target = filteredList[indexPath.section].items[indexPath.row]
         cell.textLabel?.text = target.title
         return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return list[section].title
+        return filteredList[section].title
     }
     
     func sectionIndexTitles(for tableView: UITableView) -> [String]? {
@@ -86,8 +109,32 @@ extension CitySelectionViewController: UITableViewDelegate, UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-        return list.firstIndex {
+        return filteredList.firstIndex {
             return $0.title.uppercased() == title.uppercased()
         } ?? 0
     }
+}
+
+// MARK: - searchBar
+extension CitySelectionViewController:UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            filteredList = list
+            tableView.reloadData()
+            tableView.isHidden = filteredList.isEmpty
+            return
+        }
+        filteredList.removeAll()
+        for section in list {
+            let items = section.items.filter {
+                $0.title.lowercased().contains(searchText.lowercased())
+            }
+            if !items.isEmpty {
+                filteredList.append(Section(title: section.title, items: items))
+            }
+        }
+        tableView.reloadData()
+        tableView.isHidden = filteredList.isEmpty
+    }
+            
 }
